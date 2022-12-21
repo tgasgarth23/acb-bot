e35c673531c9cc95270ce547c07368c0d9a2d696
Trevor Gasgarth <105662534+tgasgarth23@users.noreply.github.com>
2022-12-20 21:33:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def aiBot(input):
     response = openai.Completion.create(
     model = 'text-davinci-003',
     prompt = input,
-    max_tokens = 15000
+    max_tokens = 4097
     )
     print(response)
     response = response.get('choices')[0].get('text')
